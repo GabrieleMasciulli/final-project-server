3d763df2087751d3a708cbfd8b006fdf2d2fe87a
Gabriele Masciulli <gmasciu@gmail.com>
2021-04-25 21:52:07 +0200

    Added app component to the server

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const cryptosRouter = require('./controllers/cryptos')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
+logger.info('Connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+app.use(cors)
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/cryptos', cryptosRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- models/crypto_base_info.js
+++ models/crypto_base_info.js
@@ -9,11 +9,20 @@ const metricSchema = new mongoose.Schema({
   misc_data: misc_dataSchema,
 })
 
-const CryptoBaseInfoSchema = new mongoose.Schema({
+const cryptoBaseInfoSchema = new mongoose.Schema({
   symbol: String,
   slug: String,
   name: String,
+  logo: String,
   metrics: metricSchema,
 })
 
-module.exports = mongoose.model('CryptoBaseInfo', CryptoBaseInfoSchema)
+cryptoBaseInfoSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('CryptoBaseInfo', cryptoBaseInfoSchema)
