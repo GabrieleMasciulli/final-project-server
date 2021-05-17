4f9c926d7ba4c0fed935d43ddc1391055c3dc0a8
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-17 17:10:44 +0200

    Added controller for transactions
    Main POST route adds a new position if the user didn't already have one related to a specific coin, otherwise it saves a new transaction to the current position.... each position refers to a coin and a user

---
+++ controllers/transactions.js
@@ -0,0 +1,54 @@
+/* eslint-disable quotes */
+const transactionsRouter = require('express').Router()
+const Position = require('../models/position')
+const User = require('../models/user')
+
+transactionsRouter.post('/', (req, res) => {
+  const body = req.body
+  const transaction = body.transaction
+
+  User.findOne({ _id: body.user_id }, (err, user) => {
+    if (err) {
+      res.status(404).send({ message: 'User not found' })
+      return
+    }
+
+    Position.findOne({
+      $and: [{ coin_id: body.coin_id }, { user: body.user_id }],
+    }).then(position => {
+      console.log(position)
+      if (!position) {
+        const position = new Position({
+          type: body.type,
+          coin_id: body.coin_id,
+          user: body.user_id,
+          transactions: [transaction],
+        })
+
+        position.save(err => {
+          if (err) {
+            res.status(500).send({ message: err })
+            return
+          }
+
+          res
+            .status(200)
+            .json({ message: 'Buy order registered successfully!' })
+        })
+        return
+      }
+
+      Position.updateOne(
+        { $and: [{ coin_id: body.coin_id }, { user: body.user_id }] },
+        { $push: { transactions: transaction } }
+      ).then(() => {
+        res.status(200).json({
+          message:
+            'New transaction registered successfully to current postion!',
+        })
+      })
+    })
+  })
+})
+
+module.exports = transactionsRouter
