ded08dfefa9b13bc3782dbae537de85ad34d739f
Gabriele Masciulli <gmasciu@gmail.com>
2021-06-08 19:04:33 +0200

    Refactored controllers with async/await

--- controllers/cryptoSearch.js
+++ controllers/cryptoSearch.js
@@ -3,54 +3,47 @@ const searchRouter = require('express').Router()
 const coinGecko = require('../utils/coingecko')
 const SearchResult = require('../models/searchResult')
 
-searchRouter.get('/first/:limit', (req, res) => {
+searchRouter.get('/first/:limit', async (req, res) => {
   const limit = parseInt(req.params.limit)
 
-  SearchResult.find({})
-    .limit(limit)
-    .then(results => {
-      res.status(200).json(results)
-    })
+  const searchResults = await SearchResult.find({}).limit(limit)
+
+  res.status(200).json(searchResults)
 })
 
 //finding all documents which container query coming from user... using regular expression to make case-unsensitive filtering
-searchRouter.get('/:query', (req, res) => {
+searchRouter.get('/:query', async (req, res) => {
   const query = req.params.query
-  SearchResult.find({
+  const searchResults = SearchResult.find({
     $or: [
       { coingecko_id: { $regex: new RegExp(query, 'i') } },
       { symbol: { $regex: new RegExp(query, 'i') } },
       { name: { $regex: new RegExp(query, 'i') } },
     ],
-  })
-    .limit(100)
-    .then(results => {
-      res.status(202).json(results)
-    })
+  }).limit(100)
+
+  res.status(200).json(searchResults)
 })
 
 //DEVELOPEMENT ONLY!!! DON'T USE
-searchRouter.post('/:page/:number', (req, res, next) => {
+searchRouter.post('/:page/:number', async (req, res) => {
   const page = req.params.page
   const number = req.params.number
 
-  coinGecko.getBasicSearchResults(page, number).then(cryptos => {
-    const alteredSearchResults = cryptos.map(crypto => {
-      const newSearchResult = new SearchResult({
-        coingecko_id: crypto.id,
-        symbol: crypto.symbol.toUpperCase(),
-        name: crypto.name,
-        logo: crypto.image,
-      })
+  const cryptos = await coinGecko.getBasicSearchResults(page, number)
 
-      newSearchResult
-        .save()
-        .then()
-        .catch(error => next(error))
-      return { newSearchResult }
+  const alteredSearchResults = cryptos.map(async crypto => {
+    const newSearchResult = new SearchResult({
+      coingecko_id: crypto.id,
+      symbol: crypto.symbol.toUpperCase(),
+      name: crypto.name,
+      logo: crypto.image,
     })
-    res.status(202).json(alteredSearchResults)
+
+    await newSearchResult.save()
+    return { newSearchResult }
   })
+  res.status(201).json(alteredSearchResults)
 })
 
 module.exports = searchRouter
--- controllers/global.js
+++ controllers/global.js
@@ -1,11 +1,10 @@
 const globalRouter = require('express').Router()
 const coingecko = require('../utils/coingecko')
 
-globalRouter.get('/', (req, res, next) => {
-  coingecko
-    .getGlobals()
-    .then(response => res.status(200).json(response.data))
-    .catch(error => next(error))
+globalRouter.get('/', async (req, res) => {
+  const globals = await coingecko.getGlobals()
+
+  res.status(200).json(globals.data)
 })
 
 module.exports = globalRouter
